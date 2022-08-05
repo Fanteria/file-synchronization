696ffa280bcc3136e7ea65fd6af7742c1fcf8bb1
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-08-05 09:52:54 +0200

    Create help option for program.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,14 @@
 #include "synchronizer.hpp"
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
-    std::cout << "Program needs source and replica folder.";
-    return 1;
-  }
+  const std::string helpText =
+      "Program for synchronize folders:\n"
+      "Usage: file-synchronization  SOURCE REPLICA [OPTION] ...\n"
+      "SOURCE is path to source directory.\n"
+      "REPLICA is path to replica directory.\n\n"
+      "Options:\n"
+      " -i, --interval\tPositive integer of iteration time in seconds.\n"
+      " -l, --log\tName of file for logging.";
   std::string logFile = "l.log";
   std::string intervalStr = "60";
   std::string source{""};
@@ -18,13 +22,18 @@ int main(int argc, char **argv) {
   bool sourceRead = false;
   bool replicaRead = false;
   for (int i = 1; i < argc; ++i) {
+    if (!std::string("-h").compare(argv[i]) ||
+        !std::string("--help").compare(argv[i])) {
+      std::cout << helpText << std::endl;
+      return 0;
+    }
     if (!std::string("-i").compare(argv[i]) ||
-        !std::string("--interval").compare(argv[i])) {
+        !std::string("--interval").compare(argv[i]))
       intervalStr = argv[++i];
-    } else if (!std::string("-l").compare(argv[i]) ||
-               !std::string("--log").compare(argv[i])) {
+    else if (!std::string("-l").compare(argv[i]) ||
+             !std::string("--log").compare(argv[i]))
       logFile = argv[++i];
-    } else if (!sourceRead) {
+    else if (!sourceRead) {
       source = argv[i];
       sourceRead = true;
     } else if (!replicaRead) {
@@ -32,8 +41,13 @@ int main(int argc, char **argv) {
       replicaRead = true;
     } else {
       std::cout << "Wrong program input." << std::endl;
+      std::cout << helpText << std::endl;
       return 1;
     }
+  }
+  if (!(sourceRead && replicaRead)) {
+    std::cout << "Program needs source and replica folder.";
+    return 1;
   }
 
   int interval;
