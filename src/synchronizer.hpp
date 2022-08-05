28d60ed05bc2a1126f5cc3d75693b041b4132ba2
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2022-08-05 10:10:30 +0200

    Create doxygen comments for synchronizer header file.

--- src/synchronizer.hpp
+++ src/synchronizer.hpp
@@ -13,13 +13,35 @@ protected:
   std::filesystem::path replica;
   Logger logger;
 
+  /**
+   * @brief Copy file if source is newer than replica
+   *
+   * @param path is path of file
+   */
   void copyFile(const std::filesystem::path &path);
 
+  /**
+   * @brief Copy dir to replica folder if not exists.
+   *
+   * @param dir is path of directory
+   */
   void copyDir(const std::filesystem::path &dir);
 
+  /**
+   * @brief Create path to file in replica folder.
+   *
+   * @param path is path to file in source folder
+   */
   inline std::string replicaPath(const std::filesystem::path &path);
 
 public:
+  /**
+   * @brief Constructor for synchronizer.
+   *
+   * @param source is path to source directory
+   * @param replica is path to replica directory
+   * @param logFile is name of file for logging, default "log.log"
+   */
   Synchronizer(const std::string &source, const std::string &replica,
                const std::string &logFile = "log.log");
 
